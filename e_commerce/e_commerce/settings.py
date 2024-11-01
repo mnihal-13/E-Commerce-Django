3de11aa6372d28f003726e476f97a339f8214db5
Mohammed Nihal <mnkv1013@gmail.com>
2024-11-01 21:46:15 +0530

    final commit

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@$=d7bccl5-q%k29ni2ozya&=+wty!1ecrn1l%%p&8^6m8d#@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/ "static"
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
