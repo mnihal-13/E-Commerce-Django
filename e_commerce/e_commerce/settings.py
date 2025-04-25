fd97ebfe6ed1b2fb8046cccd2e184915ed5e89b1
Mohammed Nihal <mnkv1013@gmail.com>
2025-04-25 19:29:43 +0530

    delete images

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@$=d7bccl5-q%k29ni2ozya&=+wty!1ecrn1l%%p&8^6m8d#@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    BASE_DIR/ "static"
+    BASE_DIR/"static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
